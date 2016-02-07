90a4e595e2164bca3e39a15cd30e6c6d44330dd2
Clement Aubry <clement.aubry@gmail.com>
2016-02-07 13:35:18 +0100

    "First commit aponevrose"

---
+++ gestionLumiere/getSunRiseShinePyEphem.py
@@ -0,0 +1,47 @@
+import ephem
+
+o=ephem.Observer()
+o.lat='48.395574'
+o.long='-4.333449'
+# o.elev=143 #elevation in meters, http://veloroutes.org/elevation/?location=kerudu+29470+loperhet&units=m
+# o.horizon = '-6' #-6=civil twilight, -12=nautical, -18=astronomical
+# o.date = '2016/6/21'
+s=ephem.Sun()
+s.compute()
+
+sunrise = ephem.localtime(o.previous_rising(s))
+sunset = ephem.localtime(o.next_setting(s))
+print('Prochain lever de soleil : ' + str(sunrise))
+print('Prochain coucher de soleil : ' + str(sunset))
+
+beg_twilight=ephem.localtime(o.previous_rising(s, use_center=True)) #Begin civil twilight
+end_twilight=ephem.localtime(o.next_setting(s, use_center=True)) #End civil twilight
+print('Lumiere a partir de : ' + str(beg_twilight))
+print('Couche a partir de : ' + str(end_twilight))
+
+
+#We relocate the horizon to get twilight times
+print('\nCorrige par rapport a l\'elevation : \n')
+o.horizon = '-6' #-6=civil twilight, -12=nautical, -18=astronomical
+
+sunrise = ephem.localtime(o.previous_rising(s))
+sunset = ephem.localtime(o.next_setting(s))
+print('Prochain lever de soleil : ' + str(sunrise))
+print('Prochain coucher de soleil : ' + str(sunset))
+
+beg_twilight=ephem.localtime(o.previous_rising(s, use_center=True)) #Begin civil twilight
+end_twilight=ephem.localtime(o.next_setting(s, use_center=True)) #End civil twilight
+print('Lumiere a partir de : ' + str(beg_twilight))
+print('Couche a partir de : ' + str(end_twilight))
+
+ouverturePorte = ephem.Date(sunrise)
+fermeturePorte = ephem.Date(ephem.Date(sunset) + 30 * ephem.minute)
+print('Ouverture poulailler : ' + str(ouverturePorte))
+print('Fermeture poulailler : ' + str(fermeturePorte))
+
+## Horaires du soleil pour aujourd'hui
+maintenant = ephem.now()
+if (maintenant > ouverturePorte):
+    print "Le soleil est leve, la porte doit etre ouverte"
+elif(maintenant > fermeturePorte):
+    print "Le soleil est couche, la porte doit etre fermee"
